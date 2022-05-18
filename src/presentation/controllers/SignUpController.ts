1d11d56d239c67b544ec2fbb4d784cfe80adbb72
gusttavosouza <gustavorobertotb@outlook.com>
2022-05-17 22:04:21 -0300

    feat: ensure SignUpController return 500 if EmailValidator throws

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,9 +1,10 @@
-import { InvalidParamError } from '../errors/InvalidParamError';
+import InvalidParamError from '../errors/InvalidParamError';
 import MissingParamError from '../errors/MissingParamError';
 import { IHttpRequest, IHttpResponse } from '../interfaces/IHttp';
 import BadRequest from '../helpers/BadRequest';
 import IController from '../interfaces/IController';
 import IEmailValidator from '../interfaces/IEmailValidator';
+import ServerError from '../errors/ServerError';
 
 class SignUpController implements IController {
   private readonly emailValidator: IEmailValidator;
@@ -13,17 +14,29 @@ class SignUpController implements IController {
   }
 
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const requiredField = ['name', 'email', 'password', 'passwordConfirmation'];
+    try {
+      const requiredField = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ];
 
-    for (const field of requiredField) {
-      if (!httpRequest.body[field]) {
-        return BadRequest(new MissingParamError(field));
+      for (const field of requiredField) {
+        if (!httpRequest.body[field]) {
+          return BadRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValid) {
-      return BadRequest(new InvalidParamError('email'));
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return BadRequest(new InvalidParamError('email'));
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
 
     return {} as IHttpResponse;
--- src/presentation/errors/InvalidParamError.ts
+++ src/presentation/errors/InvalidParamError.ts
@@ -1,4 +1,4 @@
-export class InvalidParamError extends Error {
+class InvalidParamError extends Error {
   constructor(paramName: string) {
     super(`Invalid param: ${paramName}`);
     this.name = 'InvalidParamError';
