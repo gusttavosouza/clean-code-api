89767b2a1d37a8960e11289906d36fca19108b07
gusttavosouza <gustavorobertotb@outlook.com>
2022-05-17 22:04:45 -0300

    test: ensure SignUpController return 500 if EmailValidator throws

--- src/presentation/controllers/SignUp.spec.ts
+++ src/presentation/controllers/SignUp.spec.ts
@@ -2,6 +2,7 @@ import MissingParamError from '../errors/MissingParamError';
 import InvalidParamError from '../errors/InvalidParamError';
 import SignUpController from './SignUpController';
 import IEmailValidator from '../interfaces/IEmailValidator';
+import ServerError from '../errors/ServerError';
 
 interface ISutTypes {
   sut: SignUpController;
@@ -115,5 +116,28 @@ describe('SignUp Controller', () => {
 
     sut.handle(httpRequest);
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
+  });
+
+  test('Should return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements IEmailValidator {
+      isValid(_: string): boolean {
+        throw new Error();
+      }
+    }
+
+    const emailValidatorStub = new EmailValidatorStub();
+    const sut = new SignUpController(emailValidatorStub);
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 });
