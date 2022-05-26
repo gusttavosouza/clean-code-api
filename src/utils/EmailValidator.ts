9fd7818e343249b312bf3475a8593871accd42bb
gusttavosouza <gustavorobertotb@outlook.com>
2022-05-26 17:41:41 -0300

    feat: ensure EmailValidatorAdpter return true if validator returns true

--- src/utils/EmailValidator.ts
+++ src/utils/EmailValidator.ts
@@ -1,7 +1,8 @@
+import validator from 'validator';
 import IEmailValidator from '../presentation/interfaces/IEmailValidator';
 
 export default class EmailValidatorAdapter implements IEmailValidator {
-  isValid(_: string): boolean {
-    return false;
+  isValid(email: string): boolean {
+    return validator.isEmail(email);
   }
 }
