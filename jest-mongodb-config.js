eef9486a2027b2047a2bcb989cea408b9fd8e857
gusttavosouza <gustavorobertotb@outlook.com>
2022-05-31 22:57:58 -0300

    chore: ajust config jest and mongodb

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,11 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '4.0.3',
       skipMD5: true,
     },
-    instance: {
-      dbName: 'jest',
-    },
     autoStart: false,
+    instance: {},
   },
+
+  useSharedDBForAllJestWorkers: false,
 };
