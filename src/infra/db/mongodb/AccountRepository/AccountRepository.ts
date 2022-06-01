bdc2a684fbb19d20805c243db9c5fd65bfa31093
gusttavosouza <gustavorobertotb@outlook.com>
2022-05-31 23:32:58 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/AccountRepository/AccountRepository.ts
+++ src/infra/db/mongodb/AccountRepository/AccountRepository.ts
@@ -8,8 +8,6 @@ export class AccountMongoRepository implements IAddAccountRepository {
   public async add(accountData: IAddAccountModel): Promise<IAccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
-    const account = result.ops[0];
-    const { _id, ...accountWithoutId } = account;
-    return { ...accountWithoutId, id: _id };
+    return MongoHelper.mapper(result.ops[0]);
   }
 }
