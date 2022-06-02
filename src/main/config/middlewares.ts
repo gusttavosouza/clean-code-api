dba3746c2c23704ff8fabc4413c7ee9c967d2d05
gusttavosouza <gustavorobertotb@outlook.com>
2022-06-02 17:34:52 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
+import { cors } from '../middlewares/Cors';
 import { BodyParser } from '../middlewares/BodyParser';
 
 export default (app: Express): void => {
   app.use(BodyParser);
+  app.use(cors);
 };
