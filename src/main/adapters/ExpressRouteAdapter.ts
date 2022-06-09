a6bcc6a4d899b7669323caec1a843dfcb488fdfd
gusttavosouza <gustavorobertotb@outlook.com>
2022-06-09 18:51:21 -0300

    feat: ensure ExpressRoutesAdapter returns the error message instead of the rror name

--- src/main/adapters/ExpressRouteAdapter.ts
+++ src/main/adapters/ExpressRouteAdapter.ts
@@ -8,6 +8,12 @@ export const AdapterExpressRoute = (controller: IController) => {
       body: request.body,
     };
     const httpResponse = await controller.handle(httpRequest);
-    response.status(httpResponse.statusCode).json(httpResponse.body);
+    if (httpResponse.statusCode === 200) {
+      response.status(httpResponse.statusCode).json(httpResponse.body);
+    } else {
+      response
+        .status(httpResponse.statusCode)
+        .json({ error: httpResponse.body.message });
+    }
   };
 };
