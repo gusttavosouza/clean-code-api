db20792cfe04ed1149454b0a39f7a53f9cda7696
gusttavosouza <gustavorobertotb@outlook.com>
2022-06-16 12:57:27 -0300

    test: ensure DbAuthentication return null if LoadAccountByEmailRepository return null

--- src/data/usecases/Authentication/DbAuthentication.spec.ts
+++ src/data/usecases/Authentication/DbAuthentication.spec.ts
@@ -57,4 +57,13 @@ describe('DBAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication());
     await expect(promise).rejects.toThrow();
   });
+
+  test('Should return null id LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadAccountByEmailRepositoryStub, 'load')
+      .mockReturnValueOnce(null);
+    const accessToken = await sut.auth(makeFakeAuthentication());
+    expect(accessToken).toBeNull();
+  });
 });
