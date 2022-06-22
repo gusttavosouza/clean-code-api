d5d23746dda6a44f85eb1a78f95b25a589176361
gusttavosouza <gustavorobertotb@outlook.com>
2022-06-21 21:14:32 -0300

    test: ensure BcryptAdapter returns false when compare fails

--- src/infra/criptography/BcryptAdapter.spec.ts
+++ src/infra/criptography/BcryptAdapter.spec.ts
@@ -53,4 +53,14 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'any_hash');
     expect(isValid).toBe(true);
   });
+
+  test('should return false when compare fails', async () => {
+    const sut = makeSut();
+    jest
+      .spyOn(bcrypt, 'compare')
+      .mockReturnValueOnce(new Promise(resolve => resolve(false)) as any);
+
+    const isValid = await sut.compare('any_value', 'any_hash');
+    expect(isValid).toBe(false);
+  });
 });
