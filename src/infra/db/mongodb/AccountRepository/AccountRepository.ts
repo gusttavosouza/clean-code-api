a5439011e8733d4d3408cbaa752437e173abdcc1
gusttavosouza <gustavorobertotb@outlook.com>
2022-06-23 22:52:56 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/AccountRepository/AccountRepository.ts
+++ src/infra/db/mongodb/AccountRepository/AccountRepository.ts
@@ -1,13 +1,22 @@
+import { ILoadAccountByEmailRepository } from '../../../../data/interfaces/db/ILoadAccountByEmailRepository';
 import { IAddAccountRepository } from '../../../../data/interfaces/db/IAddAccountRepository';
 import { IAccountModel } from '../../../../domain/models/Account';
 import { IAddAccountModel } from '../../../../domain/usecases/AddAccount';
 
 import { MongoHelper } from '../helpers/MongoHelper';
 
-export class AccountMongoRepository implements IAddAccountRepository {
+export class AccountMongoRepository
+  implements IAddAccountRepository, ILoadAccountByEmailRepository
+{
   public async add(accountData: IAddAccountModel): Promise<IAccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
     return MongoHelper.mapper(result.ops[0]);
+  }
+
+  public async loadByEmail(email: string): Promise<IAccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne({ email });
+    return account && MongoHelper.mapper(account);
   }
 }
