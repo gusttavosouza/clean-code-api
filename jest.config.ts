6dfd9ae8eeea9f8f33fea9478d2ac933aa402f8e
gusttavosouza <gustavorobertotb@outlook.com>
2022-06-27 21:57:36 -0300

    chore: add path mapping

--- jest.config.ts
+++ jest.config.ts
@@ -6,4 +6,12 @@ module.exports = {
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '^@data/(.*)$': '<rootDir>/src/data/$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@infra/(.*)$': '<rootDir>/src/infra/$1',
+    '^@main/(.*)$': '<rootDir>/src/main/$1',
+    '^@presentation/(.*)$': '<rootDir>/src/presentation/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
 };
