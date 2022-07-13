1e827ef3aa1661b2da305f95f30e0600602b49bf
gusttavosouza <gustavorobertotb@outlook.com>
2022-07-12 23:37:51 -0300

    test: ensure AddSurveyController calls useCases with correct values

--- src/presentation/controllers/Survey/AddSurvey/AddSurveyController.spec.ts
+++ src/presentation/controllers/Survey/AddSurvey/AddSurveyController.spec.ts
@@ -1,10 +1,16 @@
 import { BadRequest } from '@presentation/helpers/http';
-import { IHttpRequest, IValidation } from '@presentation/interfaces';
+import {
+  IHttpRequest,
+  IValidation,
+  IAddSurvey,
+  IAddSurveyModel,
+} from './AddSurveyControllerProtocols';
 import { AddSurveyController } from './AddSurveyController';
 
 interface ISutTypes {
   sut: AddSurveyController;
   validationStub: IValidation;
+  addSurveyStub: IAddSurvey;
 }
 
 const makeFakeRequest = (): IHttpRequest => ({
@@ -29,13 +35,24 @@ const makeFakeValidation = (): IValidation => {
   return new ValidationStub();
 };
 
+const makeFakeAddSurveyStub = (): IAddSurvey => {
+  class AddSurveyStub implements IAddSurvey {
+    async add(_: IAddSurveyModel): Promise<void> {
+      return new Promise(resolve => resolve());
+    }
+  }
+  return new AddSurveyStub();
+};
+
 const makeSut = (): ISutTypes => {
   const validationStub = makeFakeValidation();
-  const sut = new AddSurveyController(validationStub);
+  const addSurveyStub = makeFakeAddSurveyStub();
+  const sut = new AddSurveyController(validationStub, addSurveyStub);
 
   return {
     sut,
     validationStub,
+    addSurveyStub,
   };
 };
 
@@ -53,5 +70,13 @@ describe('AddSurvey Controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error());
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(BadRequest(new Error()));
+  });
+
+  test('Should call AddSurvey with correct values', async () => {
+    const { sut, addSurveyStub } = makeSut();
+    const addSpy = jest.spyOn(addSurveyStub, 'add');
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body);
   });
 });
