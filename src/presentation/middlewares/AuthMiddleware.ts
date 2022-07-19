77389f66beee9f751ad734a30824ada34479d347
gusttavosouza <gustavorobertotb@outlook.com>
2022-07-19 19:28:21 -0300

    feat: ensure AuthMiddlware calls LoadAccountByToke with role

--- src/presentation/middlewares/AuthMiddleware.ts
+++ src/presentation/middlewares/AuthMiddleware.ts
@@ -8,15 +8,22 @@ import {
 } from '@presentation/interfaces';
 
 export class AuthMiddleware implements IMiddleware {
-  constructor(private readonly loadAccountByToken: ILoadAccountByToken) {
+  constructor(
+    private readonly loadAccountByToken: ILoadAccountByToken,
+    private readonly role?: string,
+  ) {
     this.loadAccountByToken = loadAccountByToken;
+    this.role = role;
   }
 
   public async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
       const accessToken = httpRequest.headers?.['x-access-token'];
       if (accessToken) {
-        const account = await this.loadAccountByToken.load(accessToken);
+        const account = await this.loadAccountByToken.load(
+          accessToken,
+          this.role,
+        );
         if (account) {
           return Success({ accountId: account.id });
         }
