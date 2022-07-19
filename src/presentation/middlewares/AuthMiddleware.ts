06e9767ae1e95a79a087d6a6aeea9b0d3c2db8c2
gusttavosouza <gustavorobertotb@outlook.com>
2022-07-19 19:23:31 -0300

    feat: ensure AuthMiddlware returns 500 if LoadAccountByToken throws

--- src/presentation/middlewares/AuthMiddleware.ts
+++ src/presentation/middlewares/AuthMiddleware.ts
@@ -1,6 +1,6 @@
 import { ILoadAccountByToken } from '@domain/usecases/ILoadAccountByToken';
 import { AccessDeniedError } from '@presentation/errors';
-import { Forbidden, Success } from '@presentation/helpers/http';
+import { Forbidden, Success, InternalError } from '@presentation/helpers/http';
 import {
   IHttpRequest,
   IHttpResponse,
@@ -13,13 +13,17 @@ export class AuthMiddleware implements IMiddleware {
   }
 
   public async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token'];
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken);
-      if (account) {
-        return Success({ accountId: account.id });
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token'];
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken);
+        if (account) {
+          return Success({ accountId: account.id });
+        }
       }
+      return Forbidden(new AccessDeniedError());
+    } catch (error) {
+      return InternalError(error);
     }
-    return Forbidden(new AccessDeniedError());
   }
 }
