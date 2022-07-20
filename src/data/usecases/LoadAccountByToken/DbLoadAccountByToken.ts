2065ea863c1c6b02e9613d0f9814f1f7b834830a
gusttavosouza <gustavorobertotb@outlook.com>
2022-07-19 23:48:06 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/data/usecases/LoadAccountByToken/DbLoadAccountByToken.ts
+++ src/data/usecases/LoadAccountByToken/DbLoadAccountByToken.ts
@@ -16,7 +16,7 @@ export class DbLoadAccountByToken implements ILoadAccountByToken {
     const token = await this.decrypter.decrypt(accessToken);
     if (token) {
       const account = await this.loadAccountByTokenRepository.loadByToken(
-        token,
+        accessToken,
         role,
       );
       if (account) {
--- src/main/routes/SurveyRoutes.test.ts
+++ src/main/routes/SurveyRoutes.test.ts
@@ -2,8 +2,12 @@ import request from 'supertest';
 import { Collection } from 'mongodb';
 import { MongoHelper } from '@infra/db/mongodb/helpers/MongoHelper';
 import app from '@main/config/app';
+import { sign } from 'jsonwebtoken';
+import env from '@main/config/env';
 
 let surveyCollections: Collection;
+let accountCollections: Collection;
+
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
@@ -15,6 +19,8 @@ describe('Survey Routes', () => {
 
   beforeEach(async () => {
     surveyCollections = await MongoHelper.getCollection('surveys');
+    await surveyCollections.deleteMany({});
+    accountCollections = await MongoHelper.getCollection('accounts');
     await surveyCollections.deleteMany({});
   });
 
@@ -35,6 +41,46 @@ describe('Survey Routes', () => {
           ],
         })
         .expect(403);
+    });
+
+    test('Should return 204 if valid token', async () => {
+      const res = await accountCollections.insertOne({
+        name: 'Gustavo',
+        email: 'gustavo@gmail.com',
+        password: '123456',
+        role: 'admin',
+      });
+
+      const id = res.ops[0]._id;
+      const accessToken = sign({ id }, env.jwtSecret);
+
+      await accountCollections.updateOne(
+        {
+          _id: id,
+        },
+        {
+          $set: {
+            accessToken,
+          },
+        },
+      );
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com',
+            },
+            {
+              answer: 'Answer 2',
+            },
+          ],
+        })
+        .expect(204);
     });
   });
 });
