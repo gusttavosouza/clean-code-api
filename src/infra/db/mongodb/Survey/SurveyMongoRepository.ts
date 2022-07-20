84706b42ea54893119ecb2b43de45a1e8de1afcb
gusttavosouza <gustavorobertotb@outlook.com>
2022-07-20 19:09:20 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/Survey/SurveyMongoRepository.ts
+++ src/infra/db/mongodb/Survey/SurveyMongoRepository.ts
@@ -1,10 +1,20 @@
 import { IAddSurveyModel } from '@domain/usecases/AddSurvey';
 import { IAddSurveyRepository } from '@data/interfaces/db/Survey/IAddSurveyRepository';
+import { ILoadSurveysRepository } from '@data/interfaces/db/Survey/ILoadSurveysRepository';
+import { ISurveyModel } from '@domain/models/Survey';
 import { MongoHelper } from '../helpers/MongoHelper';
 
-export class SurveyMongoRepository implements IAddSurveyRepository {
+export class SurveyMongoRepository
+  implements IAddSurveyRepository, ILoadSurveysRepository
+{
   public async add(surveyData: IAddSurveyModel): Promise<void> {
     const surveyCollections = await MongoHelper.getCollection('surveys');
     await surveyCollections.insertOne(surveyData);
+  }
+
+  public async loadAll(): Promise<ISurveyModel[]> {
+    const surveyCollections = await MongoHelper.getCollection('surveys');
+    const surveys: ISurveyModel[] = await surveyCollections.find({}).toArray();
+    return surveys;
   }
 }
