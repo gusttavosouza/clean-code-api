727c0f315f2826a6868e2065b5a4c04d5404688a
gusttavosouza <gustavorobertotb@outlook.com>
2022-08-18 21:35:24 -0300

    test: ensure SaveSurveyResultController with thorws

--- src/presentation/controllers/SurveyResult/SaveSurveyResult/SaveSurveyResultController.spec.ts
+++ src/presentation/controllers/SurveyResult/SaveSurveyResult/SaveSurveyResultController.spec.ts
@@ -148,4 +148,15 @@ describe('SaveSurveyResult Controller', () => {
       answer: 'any_answer',
     });
   });
+
+  test('Should return 500 SaveSurveyResult throws', async () => {
+    const { sut, saveSurveyResultStub } = makeSut();
+    jest
+      .spyOn(saveSurveyResultStub, 'save')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(InternalError(new Error()));
+  });
 });
