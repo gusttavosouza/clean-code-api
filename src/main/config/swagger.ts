62692f1ae4e5767206e616f92dd03ae002e78c31
gusttavosouza <gustavorobertotb@outlook.com>
2022-08-30 22:54:36 -0300

    feat: apply no-cache on swagger api

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -1,7 +1,8 @@
 import swaggerConfig from '@main/docs';
+import { NoCache } from '@main/middlewares/NoCache';
 import { Express } from 'express';
 import { serve, setup } from 'swagger-ui-express';
 
 export default (app: Express): void => {
-  app.use('/api/docs', serve, setup(swaggerConfig));
+  app.use('/api/docs', NoCache, serve, setup(swaggerConfig));
 };
